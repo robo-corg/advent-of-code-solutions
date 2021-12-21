7d2c086346b888b48d65f8f2665d35f9d7595d37
Andrew <amcharg@gmail.com>
2021-12-20 23:12:18 -0800

    Day 21

---
+++ day-21/src/main.rs
@@ -0,0 +1,144 @@
+use std::{io::{self, BufRead}, collections::HashMap};
+
+use itertools::iproduct;
+
+type Input = Vec<i32>;
+
+fn parse_input(mut reader: impl BufRead) -> Input {
+    unimplemented!()
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+struct Game {
+    cur_player: usize,
+    player_score: [i32; 2],
+    player_pos: [i32; 2]
+}
+
+impl Game {
+    fn new(player_pos: [i32; 2]) -> Self {
+        Game {
+            cur_player: 0,
+            player_pos,
+            player_score: [0, 0]
+        }
+    }
+}
+
+fn play_part2(game: Game, possible_rolls: &Vec<i32>, memoize: &mut HashMap<Game, [usize; 3]>) -> [usize; 3] {
+    assert!(game.player_score[0] < 21);
+    assert!(game.player_score[1] < 21);
+
+    if let Some(cached) = memoize.get(&game) {
+        return *cached;
+    }
+
+    let mut player_wins = [0; 3];
+
+    let player_num = game.cur_player;
+
+    for roll in possible_rolls.iter().copied() {
+        //dbg!(roll_player_1, roll_player_2);
+        let mut game_fork = game.clone();
+
+
+        game_fork.player_pos[player_num] = (game_fork.player_pos[player_num] + roll) % 10;
+        game_fork.player_score[player_num] += game_fork.player_pos[player_num] + 1;
+
+
+        if game_fork.player_score[player_num] >= 21 {
+            player_wins[player_num] += 1;
+        }
+        else {
+            game_fork.cur_player = 1 - game_fork.cur_player;
+            let all_outcomes = play_part2(game_fork, possible_rolls, memoize);
+
+            for player_num in 0..2 {
+                player_wins[player_num] += all_outcomes[player_num];
+            }
+        }
+    }
+
+    memoize.insert(game, player_wins);
+
+    player_wins
+}
+
+fn main() {
+    let start_positions = [
+        6,
+        2
+    ];
+
+    let mut player_score = [0; 2];
+    let mut player_pos = start_positions;
+
+    let mut rolls = 0;
+
+
+    let mut maybe_winning_player = None;
+
+    {
+        let mut dice = (0..).map(|roll| {
+            rolls += 1;
+            (roll % 100) + 1
+        });
+
+        'game: loop {
+            for player_num in 0..2 {
+                let total: i32 = dice.by_ref().take(3).sum();
+                dbg!(total);
+                player_pos[player_num] = (player_pos[player_num] + total) % 10;
+                player_score[player_num] += player_pos[player_num] + 1;
+                dbg!(player_score[player_num]);
+
+                if player_score[player_num] >= 1000 {
+                    maybe_winning_player = Some(player_num);
+                    break 'game;
+                }
+            }
+        }
+    }
+
+    dbg!(player_score, rolls);
+
+    println!("{}", rolls*player_score[1-maybe_winning_player.unwrap()]);
+
+    let possible_rolls_part2: Vec<i32> = iproduct!(1..4, 1..4, 1..4).map(|(a, b, c)| { a + b + c}).collect();
+
+    //let possible_rolls_part2: Vec<i32> = iproduct!(1..4).map(|a| { a }).collect();
+
+    dbg!(&possible_rolls_part2);
+
+    let mut memoized = HashMap::new();
+
+    dbg!(start_positions);
+
+    let win_counts = play_part2(
+        Game::new(start_positions),
+        &possible_rolls_part2,
+        &mut memoized
+    );
+
+    dbg!(win_counts);
+}
+
+#[cfg(test)]
+mod test {
+    use std::io::Cursor;
+
+    use crate::{parse_input, Input};
+
+    fn get_test_input() -> Input {
+        let test_data_str = include_str!("../test_input.txt");
+
+        let test_data_reader = Cursor::new(test_data_str.to_owned());
+
+        parse_input(test_data_reader)
+    }
+
+    #[test]
+    fn test_parse() {
+        let test_data = get_test_input();
+    }
+}
