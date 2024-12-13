196d57a422940fe154b72c4162c133dff40b3963
Andrew McHarg <amcharg@gmail.com>
2024-12-12 23:39:14 -0800

    Day 12 update and day 13

--- year-2024/day-12/src/main.rs
+++ year-2024/day-12/src/main.rs
@@ -1,9 +1,12 @@
 use std::collections::{HashMap, HashSet};
 use std::hash::Hash;
 use std::io::{self, BufRead};
-use std::mem;
+use std::{i32, mem};
 use anyhow::{bail, Result};
 use ndarray::{s, Array2};
+use svg::Document;
+use svg::node::element::Path;
+use svg::node::element::path::Data;
 
 type Pos = nalgebra::Point2<i32>;
 type Vec2 = nalgebra::Vector2<i32>;
@@ -51,11 +54,338 @@ fn arr_get<'a, T>(a: &'a Array2<T>, p: Pos) -> &T {
 }
 
 #[derive(Debug, Default)]
-struct Plot {
+struct PlotNode {
     ty: char,
-    perimiter: i32,
-    area: i32,
-    ids: HashSet<u32>
+    pos: Pos
+
+}
+
+#[repr(u8)]
+enum EdgeType {
+    Right = 1 << 0,
+    Bottom = 1 << 1,
+    Left = 1 << 2,
+    Top = 1 << 3
+}
+
+struct EdgeTypeSet(u8);
+
+impl EdgeTypeSet {
+    fn empty() -> Self {
+        EdgeTypeSet(0)
+    }
+
+    fn insert(&mut self, poly_edge: EdgeType) {
+        self.0 |= poly_edge as u8;
+    }
+
+    fn is_empty(&self) -> bool {
+        self.0 == 0
+    }
+
+}
+
+#[derive(Debug)]
+struct Graph {
+    nodes: Vec<PlotNode>,
+    node_pos: HashMap<Pos, usize>,
+    edges: HashMap<usize, HashSet<usize>>
+}
+
+impl Graph {
+    fn build_from_map(map: Array2<Option<char>>) -> Graph {
+        let map_shape = map.shape();
+
+        let mut padded_map: Array2<Option<char>> = Array2::from_shape_fn((map_shape[0] + 2, map_shape[1] + 2), |_| None);
+
+        padded_map.slice_mut(s![1..-1, 1..-1]).assign(&map);
+
+        let padded_shape = padded_map.shape();
+
+        dbg!(&padded_map);
+
+
+        let mut nodes = Vec::new();
+        let mut node_pos= HashMap::new();
+        let mut edges: HashMap<usize, HashSet<usize>> = HashMap::new();
+
+        for y in 1..(padded_shape[1]-1) {
+            for x in 1..(padded_shape[0]-1) {
+                let pos = Pos::new(x as i32, y as i32);
+
+                if arr_get(&padded_map, pos).is_none() {
+                    continue;
+                }
+
+                let val = arr_get(&padded_map, pos).clone().unwrap();
+
+                let node_id = nodes.len();
+                nodes.push(PlotNode {
+                    ty: val,
+                    pos,
+                });
+                node_pos.insert(pos, node_id);
+
+                let mut p = 0;
+
+                for neigh_dir in [Vec2::new(-1, 0), Vec2::new(0, -1)].into_iter() {
+                    let neigh_pos = pos + neigh_dir;
+                    let maybe_neigh_val = arr_get(&padded_map, neigh_pos).clone();
+
+                    if let Some(neigh_val) = maybe_neigh_val {
+                        if neigh_val != val {
+                            continue;
+                        }
+
+                        let neigh_node_id: usize = node_pos[&neigh_pos];
+
+                        edges.entry(node_id).or_default().insert(neigh_node_id);
+                        edges.entry(neigh_node_id).or_default().insert(node_id);
+                    }
+                }
+
+            }
+        }
+
+        Graph {
+            nodes,
+            node_pos,
+            edges,
+        }
+    }
+
+    fn neighbors(&self, node_id: usize) -> Option<&HashSet<usize>> {
+        self.edges.get(&node_id)
+    }
+
+    fn is_connected_pos(&self, a: Pos, b: Pos) -> bool {
+        let a_ind = self.node_pos[&a];
+        let b_ind = if let Some(ind) = self.node_pos.get(&b) { ind } else { return false };
+
+        if let Some(neighbors) = self.edges.get(&a_ind) {
+            return neighbors.contains(&b_ind);
+        }
+
+        false
+    }
+
+    fn find_all_connected(&self, start_pos: Pos) -> HashSet<Pos> {
+        let mut fringe = vec![start_pos];
+        let mut fringe_set: HashSet<Pos> = fringe.iter().copied().collect();
+        let mut seen = HashSet::new();
+
+
+        while let Some(cur_pos) = fringe.pop() {
+            fringe_set.remove(&cur_pos);
+            seen.insert(cur_pos);
+
+            let cur_node_id = self.node_pos.get(&cur_pos).unwrap().clone();
+
+            if let Some(neighbors) = self.edges.get(&cur_node_id) {
+                for neighbor_id in neighbors.iter().copied() {
+                    let neighbor_pos = self.nodes[neighbor_id].pos;
+                    if !seen.contains(&neighbor_pos) && !fringe_set.contains(&neighbor_pos) {
+                        fringe.push(neighbor_pos);
+                        fringe_set.insert(neighbor_pos);
+                    }
+                }
+            }
+        }
+
+        seen
+    }
+
+    fn find_regions(&self) -> Vec<HashSet<Pos>> {
+        let mut regions = Vec::new();
+        let mut explored: HashSet<Pos> = HashSet::new();
+
+        for check_node in self.nodes.iter() {
+            if explored.contains(&check_node.pos) {
+                continue;
+            }
+
+            let region = self.find_all_connected(check_node.pos);
+            explored.extend(&region);
+            regions.push(region);
+        }
+
+        regions
+    }
+
+    fn is_edge(&self, pos: Pos) -> EdgeTypeSet {
+        let edge_mapping = [
+            (Vec2::new(1, 0), EdgeType::Right),
+            (Vec2::new(0, 1), EdgeType::Bottom),
+            (Vec2::new(-1, 0), EdgeType::Left),
+            (Vec2::new(0, -1), EdgeType::Top)
+        ].into_iter();
+
+        let mut edgeset = EdgeTypeSet::empty();
+
+        for (neigh_pos, poly_edge) in edge_mapping {
+            if !self.is_connected_pos(pos, pos + neigh_pos) {
+                edgeset.insert(poly_edge);
+            }
+        }
+
+        edgeset
+    }
+
+    fn find_edges(&self, region: &HashSet<Pos>) -> HashMap<Pos, Pos> {
+        let mut edges: HashMap<Pos, Pos> = HashMap::new();
+
+        for pos in region.iter().copied() {
+
+            let edge_mapping = [
+                (Vec2::new(1, 0), (Vec2::new(1, 0), Vec2::new(1, 1))),
+                (Vec2::new(0, 1), (Vec2::new(1, 1), Vec2::new(0, 1))),
+                (Vec2::new(-1, 0), (Vec2::new(0, 1), Vec2::new(0, 0))),
+                (Vec2::new(0, -1), (Vec2::new(0, 0), Vec2::new(1, 0)))
+            ].into_iter();
+
+            for (neigh_pos, poly_edge) in edge_mapping {
+                if !self.is_connected_pos(pos, pos + neigh_pos) {
+                    let a_poly_pos = pos + poly_edge.0;
+                    let b_poly_pos = pos + poly_edge.1;
+
+                    edges.insert(a_poly_pos, b_poly_pos);
+                }
+            }
+        }
+
+        edges
+    }
+}
+
+fn count_corners(region: &HashSet<Pos>) -> usize {
+    let mut corners = 0;
+
+    let mut region_end = Vec2::new(0, 0);
+    let mut region_start = Vec2::new(i32::MAX, i32::MAX);
+
+    for pos in region {
+        region_end[0] = i32::max(region_end[0], pos[0]);
+        region_end[1] = i32::max(region_end[1], pos[1]);
+
+        region_start[0] = i32::min(region_start[0], pos[0]);
+        region_start[1] = i32::min(region_start[1], pos[1]);
+    }
+
+    for y in region_start[1]-1..region_end[1]+2 {
+        for x in region_start[0]-1..region_end[0]+2 {
+            let w_pos = Pos::new(x, y);
+
+            let mask: u8 = (0..2).flat_map(|y_off| {
+                (0..2).map(move |x_off| {
+                    let off = Vec2::new(x_off, y_off);
+                    w_pos + off
+                })
+            }).enumerate().map(|(n, p)| {
+                if region.contains(&p) {
+                    1 << n
+                }
+                else {
+                    0
+                }
+            }).reduce(|acc, el|{
+                acc | el
+            }).unwrap();
+
+
+            let bit_count = mask.count_ones();
+
+            let corners_found = if bit_count == 0 {
+                0
+            }
+            else if bit_count == 1 || bit_count == 3 {
+                1
+            }
+            else {
+                match mask {
+                    0b1001 => 2,
+                    0b0110 => 2,
+                    _ => 0
+                }
+            };
+
+            corners += corners_found as usize;
+        }
+    }
+
+    corners
+}
+
+fn count_edges(region: &HashSet<Pos>) -> usize {
+    let mut edges = 0;
+
+    let mut region_end = Vec2::new(0, 0);
+    let mut region_start = Vec2::new(i32::MAX, i32::MAX);
+
+    for pos in region {
+        region_end[0] = i32::max(region_end[0], pos[0]);
+        region_end[1] = i32::max(region_end[1], pos[1]);
+
+        region_start[0] = i32::min(region_start[0], pos[0]);
+        region_start[1] = i32::min(region_start[1], pos[1]);
+    }
+
+    dbg!(region_start, region_end);
+
+    for y in (region_start[1]-1)..(region_end[1]+2) {
+        for x in (region_start[0]-1)..(region_end[0]+2) {
+            let w_pos = Pos::new(x, y);
+
+            dbg!(w_pos);
+
+            let mask: u8 = (0..2).flat_map(|y_off| {
+                (0..2).map(move |x_off| {
+                    let off = Vec2::new(x_off, y_off);
+                    w_pos + off
+                })
+            }).enumerate().map(|(n, p)| {
+
+                //dbg!(&p);
+                if region.contains(&p) {
+                    1 << n
+                }
+                else {
+                    0
+                }
+            }).reduce(|acc, el|{
+                acc | el
+            }).unwrap();
+
+            if w_pos == Pos::new(2, 1) {
+                dbg!(mask);
+            }
+
+            if mask & 0b0001 != 0 {
+                if mask & 0b0100 == 0  {
+                    edges += 1;
+                }
+
+                if mask & 0b0010 == 0 {
+                    edges += 1;
+                }
+
+                // let m = mask | 0b10011111;
+                // let found_edges = m.count_zeros();
+                // dbg!(found_edges);
+                // edges += found_edges;
+            }
+            else {
+                if mask & 0b0100 != 0  {
+                    edges += 1;
+                }
+
+                if mask & 0b0010 != 0  {
+                    edges += 1;
+                }
+            }
+        }
+    }
+
+    edges as usize
 }
 
 fn main() -> Result<()> {
@@ -65,163 +395,134 @@ fn main() -> Result<()> {
         parse_input(stdin_lock)?
     };
 
-    let map_shape = map.shape();
+    let graph = Graph::build_from_map(map);
 
-    let mut padded_map: Array2<Option<char>> = Array2::from_shape_fn((map_shape[0] + 2, map_shape[1] + 2), |_| None);
-
-    padded_map.slice_mut(s![1..-1, 1..-1]).assign(&map);
-
-    let padded_shape = padded_map.shape();
-
-    dbg!(&padded_map);
-
-    let mut plot_ids: HashMap<Pos, u32> = HashMap::new();
-    let mut plots: Vec<Plot> = Vec::new();
-
-    for y in 1..(padded_shape[1]-1) {
-        for x in 1..(padded_shape[0]-1) {
-            let pos = Pos::new(x as i32, y as i32);
-
-            if arr_get(&padded_map, pos).is_none() {
-                continue;
-            }
-
-            let val = arr_get(&padded_map, pos).clone().unwrap();
-            let mut found_plot_id = None;
-
-            let mut p = 0;
-
-            for neigh_dir in [Vec2::new(1, 0), Vec2::new(0, 1), Vec2::new(-1, 0), Vec2::new(0, -1)].into_iter() {
-                let neigh_pos = pos + neigh_dir;
-                let maybe_neigh_val = arr_get(&padded_map, neigh_pos).clone();
-
-                if let Some(neigh_val) = maybe_neigh_val {
-                    if neigh_val != val {
-                        p += 1;
-                    }
-                    else {
-                        let plot_id = plot_ids.entry(neigh_pos).or_insert_with(|| {
-                            let id = plots.len() as u32;
-                            plots.push(Plot {
-                                ty: val,
-                                perimiter: 0,
-                                area: 0,
-                                ids: vec![id].into_iter().collect(),
-                            });
-                            id
-                        }).clone();
-
-                        if let Some(existing_plot_id) = found_plot_id {
-                            if existing_plot_id != plot_id {
-                                let mut plot = &mut plots[plot_id as usize];
-                                plot.ids.insert(existing_plot_id);
-                            }
-                        }
-
-                        plot_ids.insert(pos,  plot_id);
-                        found_plot_id = Some(plot_id);
-                        plots[plot_id as usize].ty = val;
-                    }
-                }
-                else {
-                    p += 1;
-                }
-            }
-
-            let plot_id = if let Some(found_plot_id) = found_plot_id {
-                found_plot_id
-            } else {
-                let id = plots.len() as u32;
-                plots.push(Plot::default());
-                plot_ids.insert(pos,  id);
-                id
-            };
-
-            let mut plot = &mut plots[plot_id as usize];
-
-            plot.ty = val;
-            plot.area += 1;
-            plot.perimiter += p;
-            plot.ids.insert(plot_id);
-        }
-    }
-
-    let mut connected_plots: HashMap<u32, HashSet<u32>> = HashMap::new();
+    dbg!(&graph);
 
 
-    for plot in plots.iter() {
-        for id in plot.ids.iter() {
-            let connected = connected_plots.entry(*id).or_default();
-            connected.extend(&plot.ids);
-        }
-    }
+    dbg!("Finding regions...");
+
+    let regions = graph.find_regions();
+
+    dbg!("Finding edges...");
+
+    dbg!(regions.len());
+
+    let mut part1_cost= 0;
+    let mut part2_cost = 0;
 
 
-    let keys: Vec<u32> = connected_plots.keys().copied().collect();
-
-    dbg!("Closure");
-
-    loop {
-        let mut updates = 0;
-
-        for plot_id in keys.iter().copied() {
-            let connected = connected_plots.entry(plot_id).or_default().clone();
-
-            for connected_id in connected.iter().copied() {
-                let conns = connected_plots.entry(connected_id).or_default();
-
-                let old_len = conns.len();
-
-                conns.extend(&connected);
-
-                if old_len != conns.len() {
-                    updates += 1;
-                }
-            }
-        }
-
-        dbg!(updates);
-
-        if updates == 0 {
-            break;
-        }
-    }
-
-    dbg!("merging...");
+    for region in regions.iter() {
+        let ty = {
+            let node_id = graph.node_pos.get(region.iter().next().unwrap()).unwrap();
+            graph.nodes[*node_id].ty
+        };
 
 
-    let mut merged_plots: Vec<Plot> = Vec::new();
-    let mut processed_plots = HashSet::new();
-    for connected_plot in connected_plots {
-        if !connected_plot.1.is_disjoint(&processed_plots) {
-            continue;
-        }
-
-        let mut plot = Plot::default();
-
-        for plot_id in connected_plot.1.iter().copied() {
-            let i = plot_id as usize;
-            plot.area += plots[i].area;
-            plot.perimiter += plots[i].perimiter;
-            plot.ty = plots[i].ty;
-        }
-
-        merged_plots.push(plot);
-        processed_plots.extend(connected_plot.1);
-    }
+        //let edges = graph.find_edges(region);
+        dbg!(region);
 
 
-    let part1_cost: i32 = merged_plots.iter().map(|plot| plot.area * plot.perimiter).sum();
+        let area = region.len();
+        let perimiter = count_edges(region);
+        let sides = count_corners(region);
 
-    for plot in merged_plots.iter() {
-        if plot.area == 0 && plot.perimiter == 0 {
-            continue;
-        }
+        println!("{} * {} sides: {}", area, perimiter, sides);
 
-        println!("{} {} * {}", plot.ty, plot.area, plot.perimiter);
+        part1_cost += area * perimiter;
+        part2_cost += area * sides;
+
+        //dbg!(&edges);
+
+        // for edge in edges.iter() {
+        //     println!("{:?} -> {:?}", edge.0, edge.1);
+        // }
+
     }
 
     println!("part1: {}", part1_cost);
+    println!("part2: {}", part2_cost);
+
+
+    // XXX
+    //  X
+
+    // let mut connected_plots: HashMap<u32, HashSet<u32>> = HashMap::new();
+
+
+    // for plot in plots.iter() {
+    //     for id in plot.ids.iter() {
+    //         let connected = connected_plots.entry(*id).or_default();
+    //         connected.extend(&plot.ids);
+    //     }
+    // }
+
+
+    // let keys: Vec<u32> = connected_plots.keys().copied().collect();
+
+    // dbg!("Closure");
+
+    // loop {
+    //     let mut updates = 0;
+
+    //     for plot_id in keys.iter().copied() {
+    //         let connected = connected_plots.entry(plot_id).or_default().clone();
+
+    //         for connected_id in connected.iter().copied() {
+    //             let conns = connected_plots.entry(connected_id).or_default();
+
+    //             let old_len = conns.len();
+
+    //             conns.extend(&connected);
+
+    //             if old_len != conns.len() {
+    //                 updates += 1;
+    //             }
+    //         }
+    //     }
+
+    //     dbg!(updates);
+
+    //     if updates == 0 {
+    //         break;
+    //     }
+    // }
+
+    // dbg!("merging...");
+
+
+    // let mut merged_plots: Vec<Plot> = Vec::new();
+    // let mut processed_plots = HashSet::new();
+    // for connected_plot in connected_plots {
+    //     if !connected_plot.1.is_disjoint(&processed_plots) {
+    //         continue;
+    //     }
+
+    //     let mut plot = Plot::default();
+
+    //     for plot_id in connected_plot.1.iter().copied() {
+    //         let i = plot_id as usize;
+    //         plot.area += plots[i].area;
+    //         plot.perimiter += plots[i].perimiter;
+    //         plot.ty = plots[i].ty;
+    //     }
+
+    //     merged_plots.push(plot);
+    //     processed_plots.extend(connected_plot.1);
+    // }
+
+
+    // let part1_cost: i32 = merged_plots.iter().map(|plot| plot.area * plot.perimiter).sum();
+
+    // for plot in merged_plots.iter() {
+    //     if plot.area == 0 && plot.perimiter == 0 {
+    //         continue;
+    //     }
+
+    //     println!("{} {} * {}", plot.ty, plot.area, plot.perimiter);
+    // }
+
+    // println!("part1: {}", part1_cost);
 
     Ok(())
 }
---
+++ year-2024/day-13/src/main.rs
@@ -0,0 +1,168 @@
+use std::collections::{HashMap, HashSet};
+use std::io::{self, BufRead};
+use anyhow::Result;
+use nalgebra::Matrix2;
+use regex::Regex;
+
+type Pos = nalgebra::Point2<i64>;
+type Vec2 = nalgebra::Vector2<i64>;
+
+#[derive(Debug)]
+struct ClawMachine {
+    button_a: Vec2,
+    button_b: Vec2,
+    prize: Pos
+}
+
+type Input = Vec<ClawMachine>;
+
+fn parse_button(s: &str) -> Vec2 {
+    let re = Regex::new(r"X\+(\d+), Y\+(\d+)").unwrap();
+
+    let (_, [x_s, y_s]) = re.captures(s).unwrap().extract();
+
+    Vec2::new(
+        i64::from_str_radix(x_s, 10).unwrap(),
+        i64::from_str_radix(y_s, 10).unwrap()
+    )
+}
+
+fn parse_prize(s: &str) -> Pos {
+    dbg!(s);
+    let re = Regex::new(r"X\=(\d+), Y\=(\d+)").unwrap();
+
+    let (_, [x_s, y_s]) = re.captures(s).unwrap().extract();
+
+    Pos::new(
+        i64::from_str_radix(x_s, 10).unwrap(),
+        i64::from_str_radix(y_s, 10).unwrap()
+    )
+}
+
+fn parse_input(mut reader: impl BufRead) -> Result<Input> {
+    let mut claw_machines = Vec::new();
+
+    let mut lines = reader.lines();
+
+    while let Some(line) = lines.next() {
+        let button_a = parse_button(&line.unwrap());
+
+        let button_b = parse_button(&lines.next().unwrap().unwrap());
+
+        let prize = parse_prize(&lines.next().unwrap().unwrap());
+
+        claw_machines.push(ClawMachine {
+            button_a,
+            button_b,
+            prize,
+        });
+
+        let _ = lines.next();
+    }
+
+    Ok(claw_machines)
+}
+
+//
+
+fn main() -> Result<()> {
+    let map = {
+        let stdin = io::stdin();
+        let stdin_lock = stdin.lock();
+        parse_input(stdin_lock)?
+    };
+
+    dbg!(&map);
+
+    let token_cost = Vec2::new(3, 1);
+
+    let mut part1_cost = 0;
+
+    for claw_machine in map.iter() {
+        let mat = Matrix2::from_columns(&[claw_machine.button_a, claw_machine.button_b]);
+
+        let mat_f = mat.cast::<f64>();
+        let prize_f = claw_machine.prize.cast::<f64>().coords;
+
+        let decomp = mat_f.clone().lu();
+        let x = decomp.solve(&prize_f).expect("Linear resolution failed.");
+
+        let sol = Vec2::new(
+            f64::round(x[0]) as i64,
+            f64::round(x[1]) as i64,
+        );
+
+        let should_be_prize = mat * sol;
+
+        if should_be_prize == claw_machine.prize.coords {
+            let cost = sol.dot(&token_cost);
+            //println!("{:?} : {:?} cost: {}", sol, claw_machine.prize, cost);
+
+            part1_cost += cost;
+        }
+
+        //dbg!(x);
+    }
+
+
+
+    println!("part1: {}", part1_cost);
+
+    let mut part2_cost = 0;
+
+    for claw_machine in map.iter() {
+        let mat = Matrix2::from_columns(&[claw_machine.button_a, claw_machine.button_b]);
+
+        let adjusted_prize = claw_machine.prize + Vec2::new(10000000000000, 10000000000000);
+
+        let mat_f = mat.cast::<f64>();
+        let prize_f = adjusted_prize.cast::<f64>().coords;
+
+
+        //let mat_invert = mat_f.try_inverse().unwrap();
+
+        //let x = mat_invert * prize_f;
+        let decomp = mat_f.clone().lu();
+        let x = decomp.solve(&prize_f).expect("Linear resolution failed.");
+
+        let sol = Vec2::new(
+            f64::round(x[0]) as i64,
+            f64::round(x[1]) as i64,
+        );
+
+        //dbg!(sol)
+
+        let should_be_prize = mat * sol;
+
+        if should_be_prize == adjusted_prize.coords {
+            let cost = sol.dot(&token_cost);
+            //println!("{:?} : {:?} cost: {}", sol, claw_machine.prize, cost);
+
+            part2_cost += cost;
+        }
+
+        //dbg!(x);
+    }
+
+
+    println!("part2: {}", part2_cost);
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{parse_button, parse_prize, Pos, Vec2};
+
+    #[test]
+    fn test_parse_button() {
+        let button = parse_button("Button A: X+94, Y+34");
+        assert_eq!(button, Vec2::new(94, 34));
+    }
+
+    #[test]
+    fn test_parse_prize() {
+        let button = parse_prize("Prize: X=18641, Y=10279");
+        assert_eq!(button, Pos::new(18641, 10279));
+    }
+}
